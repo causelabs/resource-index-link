905a641fa226a2d4191527bcae6521a4fe369821
Vladimir <vladimir.bubar@kosmoskosmos.de>
2020-07-24 12:49:58 +0200

    [UPDATE] Field id replaced with field.attribute

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,7 +1,7 @@
 <template>
     <default-field :field="field" :errors="errors">
         <template slot="field">
-            <input :id="field.name" type="text"
+            <input :id="field.attribute" type="text"
                 class="w-full form-control form-input form-input-bordered"
                 :class="errorClasses"
                 :placeholder="field.name"
